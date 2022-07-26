048fa9ed7be2a5a65e80a6005878869d1b9d1464
karuniawan.kaka <karuniawankaka80@mail.ugm.ac.id>
2022-07-26 23:33:24 +0700

    ajar react Prawito - API POST

--- src/container/BlogPost/BlogPost.js
+++ src/container/BlogPost/BlogPost.js
@@ -5,17 +5,33 @@ import axios from "axios";
 
 class BlogPost extends Component {
     state = {
-        post : []
+        post : [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        }
     }
 
     getPostAPI = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then ((res)=>{
             // console.log(res.data);
             this.setState({
                 post: res.data
             })
         })      
+    }
+
+    postDataToAPI = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost)
+        .then((res)=>{
+            console.log(res);
+            this.getPostAPI();
+        }, (err) => {
+            console.log('error:',err);
+        })
     }
 
     handleRemove = (data) => {
@@ -25,17 +41,22 @@ class BlogPost extends Component {
         })
     }
 
+    handleFormChange = (event) => {
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timeStamp = new Date().getTime();
+        formBlogPostNew['id'] = timeStamp;
+        formBlogPostNew[event.target.name] = event.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+    }
+
+    handleSubmit = () => {
+        this.postDataToAPI();
+    }
+
     componentDidMount(){
         this.getPostAPI();
-
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        // .then(response => response.json())
-        // .then(json => {
-        //     console.log(json);
-        //     this.setState({
-        //         post: json
-        //     });
-        // })
     }
 
     render(){
@@ -43,8 +64,13 @@ class BlogPost extends Component {
             <Fragment>
             <div className="wrapper">
                 <p className="section-title">Blog Post</p>
-                <div className="">
-
+                <div className="form-add-post">
+                    <p className="section-title">Add Content</p>
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" placeholder="add title" onChange={this.handleFormChange}/>
+                    <label htmlFor="body">Content</label>
+                    <textarea name="body" id="body" cols="30" rows="10" placeholder="add content" onChange={this.handleFormChange}/>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
                 </div>
                 {
                     this.state.post.map(post => {
